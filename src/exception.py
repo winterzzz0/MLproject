39089394bc342f39a2f863e70e1836814f0584b6
winterzzz <abdalrhmanmicro1@gmail.com>
2025-04-27 10:55:45 -0700

    added the nootbook

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
 import logging
+import src.logger
 
 def error_message_details(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info() #traceback
@@ -22,4 +23,5 @@ if __name__ == "__main__": #making sure it works
     try:
         a = 1/0
     except Exception as e:
+        logging.info("Divided by zero")
         raise CustomException(e,sys)
